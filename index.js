f8a943e0c14da4ff005fbe79657b09013d61ea09
Jan Decker <jan.decker@student.kit.edu>
2025-10-18 16:43:24 +0200

    added a function to dynamically generate posts and render them

--- index.js
+++ index.js
@@ -28,3 +28,53 @@ const posts = [
     }
 ]
 
+
+
+const containerEl = document.querySelector(".container")
+
+/**
+ * Generates the HTML string for a single post
+ * @param {object} postData - An object from the posts array
+ * @returns {string} The complete HTML string for the post section
+ */
+function getPostHtml(postData) {
+    // The path starts with './' to correctly resolve the relative path from index.html
+    return `
+        <section>
+            <div class="post-header">
+                <img src="./${postData.avatar}" class="user-img">
+                <div>
+                    <p class="bold">${postData.name}</p> 
+                    <p>${postData.location}</p>
+                </div>
+            </div>
+            
+            <div>
+                <img src="./${postData.post}" alt="Post image for ${postData.name}" class="post-img">
+            </div>
+
+            <div class="interaction">
+                <img src="./images/icon-heart.png">
+                <img src="./images/icon-comment.png">
+                <img src="images/icon-dm.png">
+            </div>
+
+            <div class="comments">
+                <p class="bold bigger">${postData.likes} likes</p>
+                <p><span class="bold">${postData.username}</span> ${postData.comment}</p>
+            </div>
+        </section>
+    `
+}
+
+// 1. Join the array into one single HTML string
+// 2. Set the innerHTML of the container to render all posts
+
+let dynamicPosts = ''
+
+for (let i = 0; i < posts.length; i++) {
+    dynamicPosts += getPostHtml(posts[i])
+}
+
+containerEl.innerHTML = dynamicPosts
+
